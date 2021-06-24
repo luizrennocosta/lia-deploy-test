0c913f9243248ed0cd3e2efe95656b562f6746dc
Talita Shiguemoto <talita.shiguemoto@loft.com.br>
2021-06-24 17:52:58 -0300

    feat: remove dot

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ all_nouns.csv:
 
 4variation_nouns.csv:
   Subset de all_nouns com palavras que são flexionadas por gênero
+
+## Streamlit
+Para rodar o Streamlit, primeiro instale as libs abaixo:
+- streamlit
+- st-annotated-text
+Depois no termina vá a pasta raiz do repositório e rode o comando `streamlit run app.py`
--- rules/BadWordsRule.py
+++ rules/BadWordsRule.py
@@ -1,16 +1,61 @@
 from rules.BaseRule import BaseRule
+import mlconjug3
+import nltk
+from nltk.stem.snowball import SnowballStemmer
+
+nltk.download("punkt")
 
 
 class BadWordsRule(BaseRule):
     def check(self, context):
-        badwords = context['badwords']
-        word = context['word']
+        badwords = context["badwords"]
+        word = context["word"]
         return self.pt_normalize(word.text.lower()) in badwords
 
     def refactor(self, context):
-        word = context['word']
-        index = context['index']
-        response = context['response']
+        def _get_infinitive_verb(word):
+            """
+            PT: Converte o pronome em verbo no infinitivo
+            EN: GConvert the noun into infinitive verb
+            """
+            stemmer = SnowballStemmer("portuguese")
+            stem = stemmer.stem(word)
+            return word[word.index(stem) : word.index(stem) + 1 + len(stem)] + "r"
+
+        after = context["after"]
+        before = context["before"]
+        word = context["word"]
+        index = context["index"]
+        response = context["response"]
+        transformed_txt = context["transformed_txt"]
 
         # response[index] = f"**{word.text}**"
         response[index] = (word.text + " ", "Flexiona Genero", "#afa")
+        verb = _get_infinitive_verb(str(word))
+        conjugator = mlconjug3.Conjugator(language="pt")
+
+        if after.pos_ == "AUX":
+            tense = after.morph.get("Tense")
+            mood = after.morph.get("Mood")
+            verbform = after.morph.get("VerbForm")
+
+            if tense == ["Past"] or tense == ["Imp"] or mood == ["Cnd"]:
+                conj_verb = conjugator.conjugate(verb).conjug_info["Indicativo"][
+                    "Indicativo pretérito imperfeito"
+                ]["3s"]
+            elif tense == ["Pres"] or verbform == ["Inf"]:
+                conj_verb = conjugator.conjugate(verb).conjug_info["Indicativo"][
+                    "Indicativo presente"
+                ]["3s"]
+            elif tense == ["Fut"]:
+                conj_verb = conjugator.conjugate(verb).conjug_info["Indicativo"][
+                    "Indicativo Futuro do Presente Simples"
+                ]["3s"]
+        else:
+            conj_verb = conjugator.conjugate(verb).conjug_info["Indicativo"][
+                "Indicativo presente"
+            ]["3s"]
+
+        transformed_txt[index] = f"**quem {conj_verb}**"
+        if before.text[0].isupper():
+            transformed_txt[index] = f"**Quem {conj_verb}**"
