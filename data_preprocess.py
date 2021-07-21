e8b273c906b9f73241c298a87e3f2742cf317586
Amanda Ferraboli <amanda.ferraboli@loft.com.br>
2021-07-21 16:47:56 -0300

    feat: add 4+ variation nouns preprocess

---
+++ data_preprocess.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+
+all_nouns = pd.read_csv("all_nouns.csv")
+
+plus_4_variation_nouns = all_nouns.query("variation >= 4")
+
+plus_4_variation_nouns.to_csv('4plus_variation_nouns.csv', index=False)
