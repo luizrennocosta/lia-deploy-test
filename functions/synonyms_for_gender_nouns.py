b0d33bfd72fe8f56e0e87bcfd04714df9b77fdc7
Amanda Ferraboli <amanda.ferraboli@loft.com.br>
2021-09-24 16:50:18 -0300

    fix: return word when a neutral synonym is not available

--- functions/synonyms_for_gender_nouns.py
+++ functions/synonyms_for_gender_nouns.py
@@ -11,19 +11,18 @@ import pandas as pd
 df = pd.read_csv("4variation_nouns.csv")
 
 def synonyms_for_gender_nouns(word):
-    if df.loc[df["noun"] == word].shape[0] != 0:
-        palavra_suporte = df.loc[df["noun"] == word].support
-        sinonimos = Search(palavra_suporte.values[0]).synonyms()
+    palavra_suporte = df.loc[df["noun"] == word].support
+    sinonimos = Search(palavra_suporte.values[0]).synonyms()
 
-        substituto_list = []
-        for sinonimo in sinonimos:
-            if sinonimo.endswith(("ante", "ente", "ista")):
-                if word[-1] == "s":
-                    substituto_list.append(sinonimo + "s")
-                else:
-                    substituto_list.append(sinonimo)
+    substituto_list = []
+    for sinonimo in sinonimos:
+        if sinonimo.endswith(("ante", "ente", "ista")):
+            if word[-1] == "s":
+                substituto_list.append(sinonimo + "s")
+            else:
+                substituto_list.append(sinonimo)
 
-        if len(substituto_list) > 0:
-            return substituto_list[0]
+    if len(substituto_list) > 0:
+        return substituto_list[0]
     else:
         return word
