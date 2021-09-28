3ef2a3c0ec466028dc01e8cac09f951643d5e543
talitashiguemoto <74730972+talitashiguemoto@users.noreply.github.com>
2021-09-28 18:00:03 -0300

    Rename Home to Início

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ from multiapp import FirstPage
 from pages import about_us, lia
 
 app = FirstPage()
-app.add_app("Home", lia.app)
+app.add_app("Início", lia.app)
 app.add_app("Sobre LIA", about_us.app)
 app.run()
