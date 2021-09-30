c61e390a7ddb55dc5d027286cd3c8a5884540a9c
Amanda Ferraboli <amanda.ferraboli@loft.com.br>
2021-09-29 23:31:10 -0300

    feat:create rules page

---
+++ pages/rules.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+def app():
+    st.title("Fluxo da Aplicação e Regras")
+    st.header("Como LIA funciona?")
+    
+    st.image("img/LIA.jpg")
+    sentence = "texto regras"
+
+    st.write(sentence)
