eac2b7c8fd371a72069df8ffc5f84459ebc1aff6
Amanda Ferraboli <amanda.ferraboli@loft.com.br>
2021-09-29 23:32:12 -0300

    feat: update app pages

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
 from multiapp import FirstPage
-from pages import about_us, lia
+from pages import about_us, lia, rules
 
 app = FirstPage()
 app.add_app("Início", lia.app)
 app.add_app("Sobre LIA", about_us.app)
+app.add_app("Fluxo e Regras", rules.app)
 app.run()
