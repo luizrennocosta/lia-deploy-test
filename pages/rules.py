856ebec8f50c6797679fef7ad794d9f677b68008
Amanda Ferraboli <amanda.ferraboli@loft.com.br>
2021-10-12 17:56:14 -0300

    feat: badwordsrule description

--- pages/rules.py
+++ pages/rules.py
@@ -4,8 +4,26 @@ import streamlit as st
 def app():
     st.title("Fluxo da Aplicação e Regras")
     st.header("Como LIA funciona?")
-    
+    st.write("O fluxo abaixo representa a arquitetura e módulos presentes em LIA. A seguir, cada uma das regras de neutralidade é detalhada.")
     st.image("img/LIA.jpg")
-    sentence = "texto regras"
+    
+    BadWords_rule = """"
+    BadWordsRule: A Regra de 'Bad Words' verifica se a palavra analisada está na lista de palavras que flexiona gênero. 
+    Se a verificação se confirmar, a refatoração primeiro busca por um sinônimo neutro da palavra usando a função synonyms_for_gender_nouns(). 
+    Uma frase que seria neutralizada usando essa etapa de refatoração, por exemplo, é:
+    "Os alunos gostam de programação" > "Estudantes gostam de programação"
+    
+    Caso um sinônimo neutro não seja encontrado, transfere-se a palavra original para a próxima função: who_w_indicativeVerb().
+    A função who_w_indicativeVerb() utiliza o processo de lemmatização para identificar o radical da palavra analisada. 
+    O lemma é passado para uma função que busca um verbo no infinitivo que seja próximo à palavra.
+    Caso seja encontrado, substitui a palavra não neutra pelo pronome "quem" seguida da ação que caracteriza essa palavra (o verbo encontrado conjugado).
+    Uma frase que seria neutralizada usando essa etapa de refatoração, por exemplo, é:
+    "Os montadores chegaram" > "Quem monta chegou"
 
-    st.write(sentence)
+    Se mesmo assim, a palavra analisada permanecer não refatorada, transfere-se a palavra original para uma terceira função: person_noun().
+    Essa função adiciona a palavra "pessoa" no devido número (plural ou singular) seguida da palavra original em concordância com a palavra pessoa.
+    Uma frase que seria neutralizada usando essa etapa de refatoração, por exemplo, é:
+    "Os engeheiros estão trabalhando" > "As pessoas engenheiras estão trabalhando"
+
+
+    st.write(BadWords_rule)
