983e1ea8f198e62c318933b7addb28fe369f4cd0
luizrennocosta <luizrennocosta@gmail.com>
2022-12-13 17:58:20 -0300

    chore: importpath

--- functions/checking_person_related_word.py
+++ functions/checking_person_related_word.py
@@ -3,7 +3,7 @@ import nltk
 nltk.download('wordnet')
 nltk.download('omw-1.4')
 from nltk.corpus import wordnet as wn
-from translate_pt_to_en import translate_pt_to_en
+from functions.translate_pt_to_en import translate_pt_to_en
 
 def check_lowest_common_hypernym(row, support_bol=True):
     """
